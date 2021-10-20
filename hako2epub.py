d6a19b5c17038b00357aabfabf8487f120317533
quantran-novobi <quantrancse@gmail.com>
2021-10-20 10:26:20 +0700

    Update v2.0.1

--- hako2epub.py
+++ hako2epub.py
@@ -18,7 +18,7 @@ THREAD_NUM = 8
 HEADERS = {
     'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36')}
 
-tool_version = '2.0.0'
+tool_version = '2.0.1'
 bs4_html_parser = 'html.parser'
 
 
